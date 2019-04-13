d9baea3718b7bd8c5113976f1807fcfe445f82e8
Shannon Williams <shannonwilliams0418@gmail.com>
2019-04-13 19:21:24 -0400

    Catalyst

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
-var audio = {    
-    init: function() {        
-    var $that = this;        
-        $(function() {            
-            $that.components.media();        
-        });    
-    },
-    components: {        
-        media: function(target) {            
-            var media = $('audio.fc-media', (target !== undefined) ? target : 'body');            
-            if (media.length) {                
-                media.mediaelementplayer({                    
-                    audioHeight: 40,
-                    features : ['playpause', 'current', 'duration', 'progress', 'volume', 'tracks', 'fullscreen'],
-                    alwaysShowControls      : true,
-                    timeAndDurationSeparator: '<span></span>',
-                    iPadUseNativeControls: true,
-                    iPhoneUseNativeControls: true,
-                    AndroidUseNativeControls: true                
-                });            
-            }        
-        },
-            
-    },
-};
-audio.init();
+const swiper = new Swiper('.swiper-container', {
+  direction: 'vertical',
+  mousewheel: {},
+  effect: 'cube',
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false
+  }
+});
